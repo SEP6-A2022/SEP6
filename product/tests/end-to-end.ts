eaa25b944785fbd4ae629ed8f56f608b543e60c9
DanielRailean <daniel.railean1@gmail.com>
2022-11-27 18:04:08 +0100

    Update end-to-end.ts

--- product/tests/end-to-end.ts
+++ product/tests/end-to-end.ts
@@ -1,10 +1,15 @@
 import fetch from "node-fetch"
 
-const homePage = "https://express-1a77154-7uexe4aknq-ew.a.run.app/"
-const moviesPage = "https://express-1a77154-7uexe4aknq-ew.a.run.app/movies"
-const moviePage = "https://express-1a77154-7uexe4aknq-ew.a.run.app/movies/6723592"
-const toplistsPage = "https://express-1a77154-7uexe4aknq-ew.a.run.app/toplists"
-const userPage = "https://express-1a77154-7uexe4aknq-ew.a.run.app/user"
+export const baseUrl = process.env.BASE_URL ?? "https://express-1a77154-7uexe4aknq-ew.a.run.app"
+
+const routesToCheck = [
+    "/",
+    "/movies",
+    "/movies/6723592",
+    "/toplists",
+    "/user"
+]
+
 
 const testOkFetched = async (url: string) => {
     const res = await fetch(url)
@@ -13,11 +18,10 @@ const testOkFetched = async (url: string) => {
 }
 
 const main =async () => {
-    await testOkFetched(homePage)
-    await testOkFetched(moviesPage)
-    await testOkFetched(moviePage)
-    await testOkFetched(toplistsPage)
-    await testOkFetched(userPage)
+    for(const route of routesToCheck)
+    {
+        await testOkFetched(`${baseUrl}${route}`)
+    }
 }
 
 main()
